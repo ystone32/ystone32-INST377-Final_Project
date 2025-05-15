d648251e6eebf877537b60820f932c9a3b7482e7
possessionponcho320 <ystone9876@gmail.com>
2025-05-15 14:57:27 -0400

    tired idk

--- public/explore_page.js
+++ public/explore_page.js
@@ -7,7 +7,42 @@ function peopleCarousel() {
         });
   }
 
+  function obesitySubmission(event) {
+    event.preventDefault(); 
+  
+    const city = document.getElementById("city_opt").value;
+    const url = `https://data.cdc.gov/resource/6vp6-wxuq.json?year=2017&cityname=${city}&measureid=OBESITY`;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (data.length > 0) {
+          const rate = data[0].data_value;
+          console.log(`whatever: ${rate}`);
+          document.getElementById("city_name").textContent = `City: ${city}`;
+          document.getElementById("obesity_rate").textContent = `Obesity Rate: ${rate}%`;
+        } else {
+          document.getElementById("city_name").textContent = `City: ${city}`;
+          document.getElementById("obesity_rate").textContent = `Obesity Rate: Data not available`;
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+
   function theCall(){
     peopleCarousel();
+    obesitySubmission();
   }
   window.onload = theCall;
